4cf64f8b9018c1e8a52771421ac3261d190a7266
unknown <neumann6994@gmail.com>
2018-06-21 18:54:31 -0500

    more

--- Imager.py
+++ Imager.py
@@ -29,49 +29,65 @@ def testdata():
 	with open("testdataclone.txt", 'wb') as f:
 		f.write(out)
 
+
 @cli.command()
-@click.option("--image",'-i', required=True)
-@click.option('--data','-d', required=True)
+@click.option('--image','-i', required=True)
+def readdeviation(image):
+	img = Image.open(image)
+	pixelmap = img.load()
+	for y in range(img.size[1]/2):
+		for x in range(img.size[0]/2):
+			pass
+
+
+
+
+@cli.command()
+@click.option("--image", '-i', required=True)
+@click.option('--data', '-d', required=True)
 def datatophoto(image, data):
 	# get image
 	oldimg = Image.open(image)
 
-	#getDAta
+	#getData
 	with open(data, 'rb') as f:
 		lines = f.read()
 	print(lines)
 	bina = StrToBin(lines)
 	print(bina)
 
-	newimg = oldimg.resize((oldimg.size[0]*2,oldimg.size[1]*2))
+	newimg = oldimg.resize((oldimg.size[0]*2, oldimg.size[1]*2))
 	
-	buildimage(image,newimg,bina)
+	buildimage(oldimg, newimg, bina)
 
 	newimg.save("newimage.png")
 
-def deviation():
-	pos = ((1,1,0,-1),(1,1,1,0))
-	neg = ((-1,-1,0,1),(-1,-1,-1,0))
 
 def CreateFile():
 	pass
+
 
 def buildimage(oldimage,newimage,data):
 	oldpixelmap = oldimage.load()
 	newpixelmap = newimage.load()
 	counter = 0 
-	for y in range(oldimage.size[0]):
-		for x in range(oldimage.size[1]):
-			red , green, blue = oldpixelmap[x,y]
-			newx = x * 2
+
+	print("Data to add to file")
+	print(data)
+
+	for y in range(oldimage.size[1]):
+		for x in range(oldimage.size[0]):
 			newy = y * 2
+			newx = x * 2
 
-			newpixelmap[x,y] = ()
+			out = pixelConverter(oldpixelmap[x,y], data[counter:counter+3])
 
-def
+			newpixelmap[newx, newy] = out.tlpixel
+			newpixelmap[newx + 1, newy] = out.trpixel
+			newpixelmap[newx, newy + 1] = out.blpixel
+			newpixelmap[newx + 1, newy + 1] = out.brpixel
 
-def readPixelDeviation():
-
+	print ()
 
 
 def BinToStr(input_str):
@@ -93,10 +109,65 @@ def ChrToBin(chra):
 
 
 def BinToChr(chra):
-	c = int(chra,2)
+	c = int(chra, 2)
 	c = str(chr(c)).encode('ascii')
 	return c
 
+class pixelConverter:
+	data = None
+	majorpixel = None
+	tlpixel = None
+	trpixel = None
+	blpixel = None
+	brpixel = None
+	
+
+	def __init__(self, pixel,data):
+		self.majorpixel = pixel
+		adddata()
+
+
+	def __init__(self, tlpixel, trpixel, blpixel, brpixel):
+		self.tlpixel = tlpixel
+		self.trpixel = trpixel
+		self.blpixel = blpixel
+		self.brpixel = brpixel
+
+	def deviation(self, bit):
+		#pos = [[1, 1, 0, -1], [1, 1, 1, 0], [1, 1, 1, -1]]
+		#neg = [[-1, -1, 0, 1], [-1, -1, -1, 0], [-1, -1, -1, 1]]
+		pos = [[1,0,0,0]]
+		neg = [[-1,0,0,0]]
+		if bit is '1':
+			ret = random.choice(pos)
+			random.shuffle(ret)
+			return ret
+		elif bit is '0':
+			ret = random.choice(neg)
+			random.shuffle(ret)
+			return ret
+		elif bit is None:
+			return 0, 0, 0, 0
+		else:
+			return None
+
+	def pulldata(self):
+
+		pass
+
+	def adddata(self):
+
+		r, g, b, h = self.majorpixel
+
+		reddev = deviation(self.data[0])
+		greendev = deviation(self.data[1])
+		bluedev = deviation(self.data[2])
+
+		self.tlpixel = (r + reddev[0], g + greendev[0], b + bluedev[0])
+		self.trpixel = (r + reddev[1], g + greendev[1], b + bluedev[1])
+		self.blpixel = (r + reddev[2], g + greendev[2], b + bluedev[2])
+		self.brpixel = (r + reddev[3], g + greendev[3], b + bluedev[3])
+		
 
 
 if __name__ == '__main__':
